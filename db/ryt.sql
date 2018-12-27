dd853a37554d9bb45d116e722ed4509d62ef29d7
Jeremy Wells <44270311+WormyGels@users.noreply.github.com>
2018-12-27 08:14:43 -0600

    Wrote proper readme, put sql db file for easy setup.

---
+++ db/ryt.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 27, 2018 at 02:58 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 5.6.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ryt2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `videos`
+--
+
+CREATE TABLE `videos` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `channel` text NOT NULL,
+  `vidstring` text NOT NULL,
+  `seed` text NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `fav` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `videos`
+--
+ALTER TABLE `videos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- readme.md
+++ readme.md
@@ -1,1 +1,22 @@
 # Random YouTube Generator w/ History
+
+A small HTML/PHP/JavaScript project for generating random YouTube videos. Uses the YouTube data API. To get an API key you must create a project on the Google Cloud Platform page and enable the "YouTube Data API v3" service. This API key gets placed in "vars.php".
+
+In addition to generating random videos, it also logs them in a database so they can be retrieved later. In previous versions of this project, this was more relevant since there was a search and favorites, but the current version does not have this.
+
+If you're looking for something that simply generates a random YouTube video, there are probably better options. This was created for a fairly niche purpose for myself and a couple friends.
+
+## Features
+* Random YouTube video generation
+* Live update on recent videos (shared rolling)
+
+## Plans
+* Improved history browsing
+* Favorites
+
+## Preview
+![Screenshot of window](/img/screenshot.png)
+
+## Libraries Used
+* jQuery
+* Moment.js
